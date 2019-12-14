c0559fb06171ac1e55257dbafeaab747a854d79f
Jahidul Islam <jahidul479@gmail.com>
2019-12-14 23:55:42 +0600

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,16 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-
-          ],
-        ),
       ),
        // This trailing comma makes auto-formatting nicer for build methods.
     );
