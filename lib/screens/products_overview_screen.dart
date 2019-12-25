243405d9908f69d58f0574f084f2123822ea5bce
Jahidul Islam <jahidul479@gmail.com>
2019-12-25 23:10:02 +0600

    init commit

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -3,18 +3,33 @@ import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
         actions: <Widget>[
-          
+          PopupMenuButton(
+            onSelected: (int selectedValue){
+              print(selectedValue);
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: 0,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: 1,
+              ),
+            ],
+          ),
         ],
       ),
       body: ProductsGrid(),
     );
   }
 }
-
