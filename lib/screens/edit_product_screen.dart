38445ebd3e9382e1656ce0198f9cbd32e179ab4e
Jahidul Islam <jahidul479@gmail.com>
2019-12-29 00:45:09 +0600

    edit product

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EditProductScreen extends StatefulWidget {
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: ,
+    );
+  }
+}
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-product';
@@ -19,13 +20,19 @@ class UserProductsScreen extends StatelessWidget {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: productsData.items.length,
-          itemBuilder: (_, i) => UserProductItem(
-            productsData.items[i].title,
-            productsData.items[i].imageUrl,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
           ),
         ),
       ),
